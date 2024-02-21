7c5a2e6bf2983f512a3d8c7722cb0456dea20710
yichen sheng <sheng30@purdue.edu>
2024-02-20 23:01:20 -0500

    add environment setup script

---
+++ env.sh
@@ -0,0 +1,11 @@
+# conda create -n pixht python=3.9  -y
+# conda activate pixht
+
+conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
+
+cd Docker/imagen && pip install . && cd - 
+
+pip install holoviews==1.17.0
+pip install --force-reinstall --user param==1.13.0 numpy==1.26.0
+pip install matplotlib pandas scipy dominate tensorboard opencv-python h5py 
+
