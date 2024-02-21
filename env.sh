e05e86972ca4d81d10dc660b850359c65ab67cb1
yichen sheng <sheng30@purdue.edu>
2024-02-20 23:49:54 -0500

    add missing package

--- env.sh
+++ env.sh
@@ -4,8 +4,11 @@
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
 
 cd Docker/imagen && pip install . && cd - 
-cd Demo/Torch_Render && pip install . && cd -
+echo "Imagen Installed"
 
-pip install matplotlib pandas scipy dominate tensorboard opencv-python h5py 
+cd Demo/Torch_Render && pip install . && cd -
+echo "Torch Render Installed"
+
+pip install matplotlib pandas scipy dominate tensorboard opencv-python h5py jupyterlab PyQt5
 pip install holoviews==1.17.0
 pip install --force-reinstall --user param==1.13.0 numpy==1.26.0
