e05820b78165d021eabcb74ab804c8e8692197ac
yichen sheng <sheng30@purdue.edu>
2024-02-21 00:00:36 -0500

    fix torch render

--- env.sh
+++ env.sh
@@ -5,10 +5,11 @@ conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit
 
 cd Docker/imagen && pip install . && cd - && echo "Imagen Installed"
 
-
 cd Demo/PixhtLab/Torch_Render && pip install . && cd - && echo "Torch Render Installed"
 
 
 pip install matplotlib pandas scipy dominate tensorboard opencv-python h5py jupyterlab PyQt5
+conda install conda-forge::meshplot -y
+
 pip install holoviews==1.17.0
 pip install --force-reinstall --user param==1.13.0 numpy==1.26.0
